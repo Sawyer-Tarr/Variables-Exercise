ef5331499d5f061dbfbe4f17cbbd7d96ee7c100b
sawyer tarr <sawyertarr@outlook.com>
2023-01-31 09:25:40 -0600

    Wrote a story

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userName = "Paralax";
+            int age = 432;
+            char planetNumber = '9';
+            bool planetStatus = true;
+            double humanDollars = 846.50;
+            decimal alienDollars = 563452.36m;
+
+
+            Console.WriteLine($"Hello, my name is {userName}. I am {age} years old. I am from the planet Aphla-{planetNumber}. " +
+                $"Did I destroy my home planet? This is {planetStatus}. But it was an accident and now I'm looking for a hiding place on your's to " +
+                $"keeping conducting my experiments. I am willing to pay {humanDollars} in human dollars or {alienDollars} in alien dollars. Thank you for " +
+                $"considering my offer. With love, {userName}."); 
+        
         }
     }
 }
